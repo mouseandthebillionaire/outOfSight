9ab92eea5ae9ebfc50f3bf5777c03609b51c9422
m <matt@mouseandthebillionaire.com>
2022-11-11 17:16:20 -0500

    Better stop stopping now. Uploaded the builds

--- Assets/Scripts/BeltManager.cs
+++ Assets/Scripts/BeltManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Random;
+using Random = UnityEngine.Random;
 
 public class BeltManager : MonoBehaviour
 {
