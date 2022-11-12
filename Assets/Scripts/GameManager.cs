24d668decb5af591051ffe7e4348a3ab15ca21be
m <matt@mouseandthebillionaire.com>
2022-11-12 07:32:12 -0500

    Menu scene to preload music and correctly spell "conveyor"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -23,5 +24,10 @@ public class GameManager : MonoBehaviour
     public void UpdateScore(int amount)
     {
         score += amount;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("ConveyorBeltSequencer");
     }
 }
